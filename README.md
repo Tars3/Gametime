8c4b71d0029c22e8be7e7467122c12a8624e2ee9
Carlos Borges <caborges3@gmail.com>
2016-04-11 09:52:34 -0700

    created readme rails g all controllers

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gametime!
+## ZOMG Carlos’ RL FTW LFG App, WTG!
+
+This is an app for getting a bunch of gamers together to game with.  Just set a game and a time (creative title right?) and wait for warriors, spartans, footballers, fellow zombie slayers to join up and play! It’s like the little girls from The Shining except on the internet, “Come play with us!”
+
+Technologies
+HTML
+CSS
+JavaScript
+jQuery
+Ruby on Rails
--- app/controllers/playtimes_controller.rb
+++ app/controllers/playtimes_controller.rb
@@ -1,4 +1,43 @@
 class PlaytimesController < ApplicationController
-  def show
+
+  def index
+    @playtimes = Playtime.all
   end
+
+  def show
+    @playtime = Playtime.find(params[:id])
+  end
+
+  def new
+    @playtime = Playtime.new
+  end
+
+  def create
+  @playtime = Playtime.new(params.require(:playtime).permit(:name))
+    if @playtime.save
+        redirect_to playtimes_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @playtime = Playtime.find(params[:id])
+  end
+
+  def update
+    @playtime = Playtime.find(params[:id])
+    if @playtime.update_attributes(params.require(:playtime).permit(:name))
+      redirect_to playtimes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @playtime = Playtime.find(params[:id])
+    @playtime.destroy
+    redirect_to playtimes_path
+  end
+
 end
